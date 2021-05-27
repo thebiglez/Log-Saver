138087efc33a3d2628d54e9b2fe69c3019e65528
thebiglez <79386460+thebiglez@users.noreply.github.com>
2021-05-26 17:19:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 from pywebcopy import save_webpage
-import urllib.request
+# import urllib.request
 
 download_folder = 'C:\\Users\\Mike Nolan\\Documents\\Documents\\Logbot'
 kwargs = {'bypass_robots': True, 'project_name': 'Logs'}
 
 
-dpm = [31, 28, 31, 30, 31, 31, 31, 31, 30, 31, 30, 31]
-year0 = 2021
-month0 = 4
-day0 = 18
-end = False
+dpm = [31, 28, 31, 30, 31, 31, 31, 31, 30, 31, 30, 31] # days per month
+year0 = 2021 # starting year
+month0 = 4 # starting month
+day0 = 18 # starting day
+end = False # program finished?
 
-for year in range(year0, 2022):
-    if year % 4 == 0:
+for year in range(year0, 2022): # cycles year
+    if year % 4 == 0: # check leap year
         dpm[1] = 29
     else:
         dpm[1] = 28
 
-    for month in range(month0, 13):
-        for day in range(day0, dpm[month - 1] + 1):
-            urlDate = str(year)
+    for month in range(month0, 13): # cycles month
+        for day in range(day0, dpm[month - 1] + 1): # cycles day
+            urlDate = str(year) # append year to string
 
-            if month < 10:
+            if month < 10: # check for zero
                 urlDate += '0'
-            urlDate += str(month)
+            urlDate += str(month) # add month to string
 
-            if day < 10:
+            if day < 10: # check for zero
                 urlDate += '0'
-            urlDate += str(day)
+            urlDate += str(day) # add day to string
 
-            if urlDate == '20210525':
+            if urlDate == '20210525': # check if final day
                 end = True
                 break
 
-            url = 'https://freenode.logbot.info/grapheneos-offtopic/' + urlDate
+            url = 'https://freenode.logbot.info/grapheneos-offtopic/' + urlDate # create URL
             print(url)
             # urllib.request.urlretrieve(url, 'OT' + urlDate + '.html')
-            save_webpage(url, download_folder, **kwargs)
+            save_webpage(url, download_folder, **kwargs) # save full webpage to download folder
 
         if end:
             break
